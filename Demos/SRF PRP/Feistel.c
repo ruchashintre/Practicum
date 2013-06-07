8d1fa8ecf5e50cb5282af076d7f6e7a5f40f394c
gundamwings <gundamwings@ubuntu.(none)>
2013-06-07 22:16:45 +0800

    modified Fesistel for logic errors when computing R

--- Demos/SRF PRP/Feistel.c
+++ Demos/SRF PRP/Feistel.c
@@ -20,14 +20,14 @@ Version 		Date 			Author			Comments
 //number of rounds
 #define NOOFROUNDS 6
 
-int blocks;
-int index_bit_length;
-unsigned int * round1table;
-unsigned int * round2table;
-unsigned int * round3table;
-unsigned int * round4table;
-unsigned int * round5table;
-unsigned int * round6table;
+static int blocks=5;
+static double index_bit_length;
+static unsigned int * round1table;
+static unsigned int * round2table;
+static unsigned int * round3table;
+static unsigned int * round4table;
+static unsigned int * round5table;
+static unsigned int * round6table;
 
 void generateRoundFunctions(unsigned char * seed, unsigned int * bufint,int blocks); 
 
@@ -46,7 +46,8 @@ int main(int argc, char *argv[])
 
 	//find the bit length  of the each element of array to get number of blocks
 	index_bit_length = log2(blocks);
-	
+	int bit = ceil(index_bit_length);
+	    	printf("bit num: %d\n",bit);
 	//declare for block indices table, and permuted indices table
 	int * blockindices;
 	int * prpblockindices;
@@ -71,22 +72,28 @@ int main(int argc, char *argv[])
 
 	//Generate 6 functions
 	printf("Round 1\n");
+	round1table = malloc(blocks*sizeof(unsigned int));
 	generateRoundFunctions(seed1,round1table,blocks);
 	printf("Round 2\n");
+	round2table = malloc(blocks*sizeof(unsigned int));
 	generateRoundFunctions(seed2,round2table,blocks);
 	printf("Round 3\n");
+	round3table = malloc(blocks*sizeof(unsigned int));
 	generateRoundFunctions(seed3,round3table,blocks);
-	printf("Round 4\n");	
+	printf("Round 4\n");
+	round4table = malloc(blocks*sizeof(unsigned int));	
 	generateRoundFunctions(seed4,round4table,blocks);
 	printf("Round 5\n");	
+	round5table = malloc(blocks*sizeof(unsigned int));
 	generateRoundFunctions(seed5,round5table,blocks);
 	printf("Round 6\n");
+	round6table = malloc(blocks*sizeof(unsigned int));
 	generateRoundFunctions(seed6,round6table,blocks);
 	printf("6 tables generated");
 
 	//using setting in the paper: change it later, to calculate a and b
-	int a = 185360;
-	int b = 185368;
+	int a = 6;
+	int b = 7;
 
 	//get the keys for permutation
 	
@@ -95,26 +102,27 @@ int main(int argc, char *argv[])
 	
 	//do this for six rounds
 	for(i=0;i<blocks;i++){
-		printf("Number of rounds");
-		prpblockindices[i]=Fe(NOOFROUNDS, a, b,i);
+		printf("Number of rounds: %d\n",i);
+		prpblockindices[i]=Fe(NOOFROUNDS, a, b,i, blocks, bit);
 	}
 	
 	for(i=0;i<blocks;i++){
-		printf("%d -> %d", blockindices[i], prpblockindices[i]);		
+		printf("%d -> %d\n", blockindices[i], prpblockindices[i]);		
 	}
 }
 
 //generate 6 different functions and store in memory
 void generateRoundFunctions(unsigned char * seed, unsigned int * bufint, int blocks)
 {
-	printf("before malloc");
+	printf("before malloc\n");
 	int err;
 	int i=0;
 	int j=0;
 
 	unsigned char buf[4];
 	
-	bufint = malloc(blocks*sizeof(unsigned int));
+	//bufint = malloc(blocks*sizeof(unsigned int));
+
 	
 	int index = 0;
 	prng_state prng; 
@@ -129,19 +137,19 @@ void generateRoundFunctions(unsigned char * seed, unsigned int * bufint, int blo
 		printf("Ready error: %s\n", error_to_string(err));
 	}
 		
-	
+
 	for(i=0;i<blocks;i++){
 			
 		fortuna_read(buf,sizeof(buf),&prng);
 		bufint[i] = *(unsigned int *)buf;	
 	}
 	
+	printf("%d blocks ", blocks);
+	for(i=0;i<blocks;i++){
 
-//printf("%d blocks ", blocks);
-	//for(i=0;i<blocks;i++){
-		
-	//	printf("%u  ",bufint[i]);
-	//}
+		printf("%u  ",bufint[i]);
+	}
+	fflush(stdout);
 
 }
 
@@ -178,51 +186,55 @@ int genkey(char *seed){
 */
 
 // buf = k = random number
-int Fe(int r, int a, int b, int m){
+int Fe(int r, int a, int b, int m, int blocks, int bit){
 	
-	printf("I am  in Main Feistel function");
-
-	int c = fe(r,a,b,m);
-	if(c < blocks)
+	int c = fe(r,a,b,m,bit);
+	printf("c=%d,blocks=%d\n",c,blocks);
+	if(c < blocks) {
 		return c;
-	else
+	}
+	else {
 		//perform cycle walking
-		return Fe(r, a, b,c);
+		return Fe(r, a, b,c, blocks, bit);
+	}
 }
 
 //computation of L , R
-int fe(int r, int a, int b, long m){
-	printf("I am  in Main Feistel function");
+int fe(int r, int a, int b, long m,int bit){
 	int tmp, j;
 	int L = (int)m % a;
 	int R = (int)m / a;
 	for(j=1;j<=r;j++){
+		int fval = applyRoundFunctions(j,R,bit);
+		printf("fval=%d\n",fval);
 		if(!(j%2)){
-			tmp = (L ^ applyRoundFunctions(j,R)) % b;
+			tmp = (L ^ fval) % b;
 		}else{
-			tmp = applyRoundFunctions(j,R) % a;
+			tmp = (L ^ fval) % a;
 		}
 		L = R;
 		R = tmp;	
 	}
 	
 	if(!(r%2)){
+		int kmn = (a*L+R);
 		return (a*L+R);
 	}else{
+		int kmn = (a*R+L);
 		return (a*R+L);
 	}
 }
 
 //get the value of the function for given R
-int applyRoundFunctions(int round, int R){
-	
+int applyRoundFunctions(int round, int R, int bit){
+	printf("bit=%d\n",bit);
 	switch(round){
-		case 1: return round1table[R];
-		case 2: return round2table[R];
-		case 3: return round3table[R];
-		case 4: return round4table[R];
-		case 5: return round5table[R];
-		case 6: return round6table[R];	
+		case 1: return round1table[R]&((1<<(bit))-1);
+		case 2: return round2table[R]&((1<<(bit))-1);
+		case 3: return round3table[R]&((1<<(bit))-1);
+		case 4: return round4table[R]&((1<<(bit))-1);
+		case 5: return round5table[R]&((1<<(bit))-1);
+		case 6: return round6table[R]&((1<<(bit))-1);	
 	}
 }
 	//print initial and final permutation
